ec536c626c3ef377986e60c0bf6fd63582ac3459
Zubair Ansari <CSC18F002@stu.smiu.edu.pk>
2023-10-16 12:34:47 +0500

    updation of manu of community

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -90,51 +90,9 @@
                                     <div x-show="community" x-cloak>
                                         <ul class="ml-4 lg:absolute lg:flex lg:flex-col lg:ml-0 lg:mt-2 lg:w-48 lg:rounded-md lg:shadow-lg lg:z-50 lg:bg-white">
                                             <li class="my-4 lg:hover:bg-gray-100 lg:my-0">
-                                                <a href="https://github.com/laravelio" class="inline-block w-full lg:px-4 lg:py-3">
+                                                <a href="https://github.com/laravelpakistan" class="inline-block w-full lg:px-4 lg:py-3">
                                                     <x-icon-github class="w-4 h-4 inline"/>
                                                     Github
-                                                </a>
-                                            </li>
-
-                                            <li class="mb-4 lg:hover:bg-gray-100 lg:mb-0">
-                                                <a href="https://twitter.com/laravelio" class="inline-block w-full lg:px-4 lg:py-3">
-                                                    <x-icon-twitter class="w-4 h-4 inline text-twitter"/>
-                                                    Twitter
-                                                </a>
-                                            </li>
-
-                                            <li class="mb-4 lg:hover:bg-gray-100 lg:mb-0">
-                                                <a href="https://laravel.com" class="inline-block w-full lg:px-4 lg:py-3">
-                                                    <img loading="lazy" src="{{ asset('images/laravel.png') }}" alt="Laravel" class="w-4 h-4 inline" />
-                                                    Laravel
-                                                </a>
-                                            </li>
-
-                                            <li class="mb-4 lg:hover:bg-gray-100 lg:mb-0">
-                                                <a href="https://laracasts.com" class="inline-block w-full lg:px-4 lg:py-3">
-                                                    <img loading="lazy" src="{{ asset('images/laracasts.png') }}" alt="Laracasts" class="w-4 h-4 inline" />
-                                                    Laracasts
-                                                </a>
-                                            </li>
-
-                                            <li class="mb-4 lg:hover:bg-gray-100 lg:mb-0">
-                                                <a href="https://laravel-news.com" class="inline-block w-full lg:px-4 lg:py-3">
-                                                    <img loading="lazy" src="{{ asset('images/laravel-news.png') }}" alt="Laravel News" class="w-4 h-4 inline" />
-                                                    Laravel News
-                                                </a>
-                                            </li>
-
-                                            <li class="mb-4 lg:hover:bg-gray-100 lg:mb-0">
-                                                <a href="https://www.laravelpodcast.com" class="inline-block w-full lg:px-4 lg:py-3">
-                                                    <img loading="lazy" src="{{ asset('images/podcast.png') }}" alt="Laravel Podcast" class="w-4 h-4 inline" />
-                                                    Podcast
-                                                </a>
-                                            </li>
-
-                                            <li class="hover:bg-gray-100">
-                                                <a href="https://ecosystem.laravel.io" class="inline-block w-full lg:px-4 lg:py-3">
-                                                    <img loading="lazy" src="{{ asset('images/laravelio-icon.svg') }}" alt="Laravel Podcast" class="w-4 h-4 inline" />
-                                                    Ecosystem
                                                 </a>
                                             </li>
                                         </ul>
