9c066b71f8014293ee0fc70b0fe910b9669544c1
laravelpakistan <145439689+laravelpakistan@users.noreply.github.com>
2024-04-23 15:40:59 +0500

    improvement

--- resources/views/emails/article_approved.blade.php
+++ resources/views/emails/article_approved.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 
-Great news! Your article: **{{ $article->title() }}** has been approved and is now live on Laravel.io.
+Great news! Your article: **{{ $article->title() }}** has been approved and is now live on Laravel Pakistan.
 
 @component('mail::button', ['url' => route('articles.show', $article->slug())])
 View Article
